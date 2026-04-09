cc6473161e18f7c8be6aeeeede1c0200db657148
Joy-Joy20 <gulpanjoy@gmail.com>
2026-04-09 18:02:56 +0800

    update project files

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
-# Temporary in-memory storage (no database)
 bookings = []
 
-# Home
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Book Appointment
 @app.route('/book', methods=['GET', 'POST'])
 def book():
     if request.method == 'POST':
@@ -28,14 +26,11 @@ def book():
 
     return render_template('book.html')
 
-# View Bookings
 @app.route('/bookings')
 def bookings_page():
     return render_template('bookings.html', bookings=bookings)
 
 
-# IMPORTANT: This is needed for deployment
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
